992cf92b5b71cf3ae328abda662f97bee305ba19
Scott Schindler <scottschindler29@gmail.com>
2018-07-24 23:11:55 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,14 +1,14 @@
-def display_board 
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
- end
+def position_taken? (board,index)
   
-   def position_taken?(position)
-  if @board[position] == "X" || @board[position] == "O"
-      true
-   else
-     false
-   end 
+  if board[index] == " " || board[index] == "" || board[index] == nil 
+  
+    return false
+    
+  elsif board[index] == "X" || board[index] == "O"
+  
+    return true
+  
+  end
+  
+  
+end
